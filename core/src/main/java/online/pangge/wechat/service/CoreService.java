e15a452b960b980453d10ed4b66ae16dd912fd6d
jimmy <m13427598892@163.com>
2017-11-07 22:37:04 +0800

    oss properties error

--- core/src/main/java/online/pangge/wechat/service/CoreService.java
+++ core/src/main/java/online/pangge/wechat/service/CoreService.java
@@ -16,6 +16,8 @@ import online.pangge.wechat.damain.message.resp.NewsMessage;
 import online.pangge.wechat.damain.message.resp.TextMessage;
 import online.pangge.wechat.util.MessageUtil;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,6 +41,7 @@ public class CoreService {
      *
      * @return xml
      */
+    private static Logger log = LoggerFactory.getLogger(CoreService.class);
     @Autowired
     private RedisUtil redisUtil;
     @Autowired
@@ -279,6 +282,7 @@ public class CoreService {
                 wrongSubjectLink.setLastUpdateDate(new Date());
                 wrongSubjectLink.setUserId(studentService.selectByWechatName(fromusername).get(0).getStunum());
                 wrongSubjects.add(wrongSubjectLink);
+                log.info("insert wrong subject with id:"+wrongSubjectLink.getSubId());
             }
         }
         wrongSubjectService.insertWrongSubjectLinks(wrongSubjects);
