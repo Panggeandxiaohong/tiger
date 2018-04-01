05544a7b40765c7574db3442f3776b582b37e2af
jimmy <m13427598892@163.com>
2018-04-01 18:24:02 +0800

    test git

--- core/src/main/java/online/pangge/exam/job/JobRunnerTest.java
+++ core/src/main/java/online/pangge/exam/job/JobRunnerTest.java
@@ -1,16 +1,13 @@
 package online.pangge.exam.job;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
 /**
  * Created by Pangge on 2017/11/5.
  */
 public class JobRunnerTest {
     public static void main(String[] args) {
-        ApplicationContext beanFactory = new FileSystemXmlApplicationContext("classpath:application.xml");
-        Job job = (Job) beanFactory.getBean("SendSubjectDetail");
-        JobExecute execute = new JobExecute();
-        execute.start(job);
+        System.out.println("start job ...");
+        for(String s : args){
+            System.out.println(s);
+        }
     }
 }
